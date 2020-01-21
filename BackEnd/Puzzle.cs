e1e530da20b2138763d4dd7c387b676d78a4a4ad
Christopher <chuijting@sogyo.nl>
2020-01-21 14:28:50 +0100

    Initial Tile

--- BackEnd/Puzzle.cs
+++ BackEnd/Puzzle.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BackEnd
 {
     public class Puzzle
     {
+        List<Tile> allTiles;
+        public Puzzle(int size)
+        {
+
+        }
     }
 }
