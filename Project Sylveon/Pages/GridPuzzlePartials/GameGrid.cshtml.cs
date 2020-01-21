1f9e48498b43d799dba1a5bcfc03205bd5459e36
Jeroen Endenburg <jendenburg@sogyo.nl>
2020-01-21 15:52:24 +0100

    Implemented system for partial/sub models

---
+++ Project Sylveon/Pages/GridPuzzlePartials/GameGrid.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace Project_Sylveon.Pages.Partials
+{
+    public class GameGridSubModel : ISubModel
+    {
+        public void OnGet(HttpRequest request, HttpResponse response)
+        {
+        }
+
+        public void OnPost(HttpRequest request, HttpResponse response)
+        {
+        }
+
+        public void OnPut(HttpRequest request, HttpResponse response)
+        {
+        }
+
+        public void OnPatch(HttpRequest request, HttpResponse response)
+        {
+        }
+
+        public void OnDelete(HttpRequest request, HttpResponse response)
+        {
+        }
+    }
+}
--- Project Sylveon/Pages/Index.cshtml.cs
+++ Project Sylveon/Pages/Index.cshtml.cs
@@ -5,11 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+using Project_Sylveon.Pages.Partials;
 
 namespace Project_Sylveon.Pages
 {
     public class IndexModel : PageModel
     {
+        public ISubModel CodeMenu { get; } = new CodeMenuSubModel();
+        public ISubModel DraggableElementsMenu { get; } = new DraggableElementsMenuSubModel();
+        public ISubModel GameGrid { get; } = new GameGridSubModel();
+
+        public ISubModel[] SubModels
+        {
+            get => new ISubModel[] {
+                CodeMenu,
+                DraggableElementsMenu,
+                GameGrid,
+            };
+        }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,7 +35,32 @@ namespace Project_Sylveon.Pages
 
         public void OnGet()
         {
+            foreach(ISubModel subModel in SubModels)
+                subModel.OnGet(Request, Response);
+        }
 
+        public void OnPost()
+        {
+            foreach(ISubModel subModel in SubModels)
+                subModel.OnPost(Request, Response);
+        }
+
+        public void OnPut()
+        {
+            foreach(ISubModel subModel in SubModels)
+                subModel.OnPut(Request, Response);
+        }
+
+        public void OnPatch()
+        {
+            foreach(ISubModel subModel in SubModels)
+                subModel.OnPatch(Request, Response);
+        }
+
+        public void OnDelete()
+        {
+            foreach(ISubModel subModel in SubModels)
+                subModel.OnDelete(Request, Response);
         }
     }
 }
