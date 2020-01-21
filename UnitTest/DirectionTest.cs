c3b7e7f7d47394ccc147e183c77237cf2a21b7a2
Joost Franssen <jfranssen@sogyo.nl>
2020-01-21 14:14:46 +0100

    Add tests for Direction

---
+++ UnitTest/DirectionTest.cs
@@ -0,0 +1,39 @@
+﻿using BackEnd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    class DirectionTest
+    {
+        [TestMethod]
+        public void LeftOfNorthIsWest()
+        {
+            Direction dir = Direction.NORTH;
+
+            Direction dirLeft = dir.Left();
+
+            Assert.AreEqual(Direction.WEST, dirLeft);
+        }
+
+        [TestMethod]
+        public void LeftOfEastIsNorth()
+        {
+            Direction dir = Direction.EAST;
+
+            Direction dirLeft = dir.Left();
+
+            Assert.AreEqual(Direction.NORTH, dirLeft);
+        }
+
+        [TestMethod]
+        public void RightOfWestIsNorth()
+        {
+            Direction dir = Direction.WEST;
+
+            Direction dirRight = dir.Right();
+
+            Assert.AreEqual(Direction.NORTH, dirRight);
+        }
+    }
+}
