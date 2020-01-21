a5ee0fb52222210eada9dba537f1780978fe0de4
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-01-21 17:38:35 +0100

    Fake code needed to test.

--- BackEnd/Puzzle.cs
+++ BackEnd/Puzzle.cs
@@ -4,5 +4,13 @@ namespace BackEnd
 {
     public class Puzzle
     {
+        public Puzzle(string level)
+        {
+        }
+        public string[][] GetState() { return new string[1][]; }
+        public ICharacter GetCharacter() { return new TestNotReal(); }
+        public bool IsFinished() { return true; }
+        public int GetParLines() { return 8; }
+
     }
 }
---
+++ BackEnd/TestNotReal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd
+{
+    public class TestNotReal : ICharacter
+    {
+        public TestNotReal()
+        {
+
+        }
+
+        public void RunCommand(Command command)
+        {
+            
+        }
+    }
+}
