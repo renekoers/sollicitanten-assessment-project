ece373fea23db0f0ee0573ee2c65aca966b457ce
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-01-22 10:38:10 +0100

    Renamed Levels->Level

--- BackEnd/Level.cs
+++ BackEnd/Level.cs
@@ -5,20 +5,20 @@ using System.Text;
 
 namespace BackEnd
 {
-    public class Levels
+    public class Level
     {
         private int[] _gridSize;
-        private int _targetLines;
+        private int _par;
         private int[][] _walls;
         private int[] _begin;
         private int[] _end;
         private int[][] _buttons;
         private int[][] _doors;
         private int[][] _boxes;
-        private Levels(int[] gridSize, int targetLines, int[][] walls, int[] begin, int[] end, int[][] buttons, int[][] doors, int[][] boxes)
+        private Level(int[] gridSize, int par, int[][] walls, int[] begin, int[] end, int[][] buttons, int[][] doors, int[][] boxes)
         {
             this._gridSize = gridSize;
-            this._targetLines = targetLines;
+            this._par = par;
             this._walls = walls;
             this._begin = begin;
             this._end = end;
@@ -26,7 +26,7 @@ namespace BackEnd
             this._doors = doors;
             this._boxes = boxes;
         }
-        private static Levels[] levels = new Levels[]{new Levels(new int[] { 3, 3 }, 8, new int[][] { new int[] { 1, 0 }, new int[] { 1, 2 } }, 
+        private static Level[] levels = new Level[]{new Level(new int[] { 3, 3 }, 8, new int[][] { new int[] { 1, 0 }, new int[] { 1, 2 } }, 
                                         new int[] { 2, 1 }, new int[] { 0, 1 }, new int[][]{new int[] { 1, 2, 0 } },
                                         new int[][]{new int[] { 1, 1, 1 } }, new int[][]{new int[] { 2, 2 } })};
         public static string Get(int level)
