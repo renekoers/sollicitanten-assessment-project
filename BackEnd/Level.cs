720b9dec502d86cef0e663a3cbcc4878a85e65a6
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-01-22 11:14:02 +0100

    Fix: JSON string of the level now contains all the data.

--- BackEnd/Level.cs
+++ BackEnd/Level.cs
@@ -7,31 +7,34 @@ namespace BackEnd
 {
     public class Level
     {
-        private int[] _gridSize;
-        private int _par;
-        private int[][] _walls;
-        private int[] _begin;
-        private int[] _end;
-        private int[][] _buttons;
-        private int[][] _doors;
-        private int[][] _boxes;
-        private Level(int[] gridSize, int par, int[][] walls, int[] begin, int[] end, int[][] buttons, int[][] doors, int[][] boxes)
+        public readonly int LevelNumber;
+        public readonly int[] GridSize;
+        public readonly int Par;
+        public readonly int[][] Walls;
+        public readonly int[] Begin;
+        public readonly int[] End;
+        public readonly int[][] Buttons;
+        public readonly int[][] Doors;
+        public readonly int[][] Boxes;
+        private Level(int level, int[] gridSize, int par, int[][] walls, int[] begin, int[] end, int[][] buttons, int[][] doors, int[][] boxes)
         {
-            this._gridSize = gridSize;
-            this._par = par;
-            this._walls = walls;
-            this._begin = begin;
-            this._end = end;
-            this._buttons = buttons;
-            this._doors = doors;
-            this._boxes = boxes;
+            this.LevelNumber = level;
+            this.GridSize = gridSize;
+            this.Par = par;
+            this.Walls = walls;
+            this.Begin = begin;
+            this.End = end;
+            this.Buttons = buttons;
+            this.Doors = doors;
+            this.Boxes = boxes;
         }
-        private static Level[] levels = new Level[]{new Level(new int[] { 3, 3 }, 8, new int[][] { new int[] { 1, 0 }, new int[] { 1, 2 } }, 
+
+        private static Level[] levels = new Level[]{new Level(1,new int[] { 3, 3 }, 8, new int[][] { new int[] { 1, 0 }, new int[] { 1, 2 } }, 
                                         new int[] { 2, 1 }, new int[] { 0, 1 }, new int[][]{new int[] { 1, 2, 0 } },
                                         new int[][]{new int[] { 1, 1, 1 } }, new int[][]{new int[] { 2, 2 } })};
         public static string Get(int level)
         {
-            return JsonConvert.SerializeObject(levels[level-1]);
+            return JsonConvert.SerializeObject(levels[level - 1]);
         }
     }
 }
