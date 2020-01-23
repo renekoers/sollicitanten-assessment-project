8ccad54c4fc60a21cee850f8a89d63bf052e9c2e
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-01-23 11:33:53 +0100

    Added new level

--- BackEnd/Level.cs
+++ BackEnd/Level.cs
@@ -17,7 +17,7 @@ namespace BackEnd
         public readonly int[][] Buttons;
         public readonly int[][] Doors;
         public readonly int[][] Boxes;
-        private Level(int level, int[] gridSize, int par, int[][] walls, int[] positionCharacter, int[] end, int[][] buttons, int[][] doors, int[][] boxes)
+        public Level(int level, int[] gridSize, int par, int[][] walls, int[] positionCharacter, int[] end, int[][] buttons, int[][] doors, int[][] boxes)
         {
             this.LevelNumber = level;
             this.GridSize = gridSize;
@@ -34,9 +34,9 @@ namespace BackEnd
                                         new int[] { 2, 1 }, new int[] { 0, 1 }, new int[][]{new int[] { 1, 2, 0 } },
                                         new int[][]{new int[] { 1, 1, 1 } }, new int[][]{new int[] { 2, 2 } }),
 
-                                        new Level(2,new int[] { 5, 5 }, 8, new int[][] { new int[] { 1, 0 }, new int[] { 1, 2 } },
-                                        new int[] { 2, 1 }, new int[] { 0, 1 }, new int[][]{new int[] { 1, 2, 0 } },
-                                        new int[][]{new int[] { 1, 1, 1 } }, new int[][]{new int[] { 2, 2 } }),
+                                        new Level(2,new int[] { 5, 5 }, 8, new int[][] { new int[] { 1, 0 }, new int[] { 1, 1 },new int[] { 1, 3 }, new int[] { 1, 4 },new int[] { 3, 0 }, new int[] { 3, 1 },new int[] { 3, 3 }, new int[] { 3, 4 } },
+                                        new int[] { 4, 2 }, new int[] { 0, 0 }, new int[][]{new int[] { 1, 4, 0 }, new int[]{2,4,4 } },
+                                        new int[][]{new int[] { 1, 1, 2 },new int[] { 2, 3, 2 } }, new int[][]{new int[] { 4, 3 }, new int[]{2,0 } }),
                                         };
         public static string Get(int level)
         {
