eb220cea40fc8ed544aa3d275012e966b0e3dd0e
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-01-23 15:56:14 +0100

    Busy....

---
+++ UnitTest/StatementTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BackEnd;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+    [TestClass]
+    class StatementTest
+    {
+        [TestMethod]
+        public void GetLevelTest()
+        {
+            Puzzle puzzle = new Puzzle(Level.Get(1));
+            ICharacter character = puzzle.Character;
+        }
+    }
+}
