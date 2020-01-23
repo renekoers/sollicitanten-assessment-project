23ff666b5688573d0dca6497ef9cbbc3b47da580
Rene <rene.g.koers@gmail.com>
2020-01-23 10:36:44 +0100

    Added IState interface

---
+++ BackEnd/IState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd
+{
+    public interface IState
+    {
+        Character Character { get; }
+
+
+    }
+}
