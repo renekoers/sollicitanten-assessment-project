17c69fda1e23845a67eb650fed23860d8598ea5b
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-01-23 11:06:39 +0100

    Added conditonal parameters and values that are needed for loops. ICharacter contains method CheckCondition.

---
+++ BackEnd/ICharacter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BackEnd.Statements;
+
+namespace BackEnd
+{
+    public interface ICharacter
+    {
+        void ExecuteCommand(Command command);
+        bool CheckCondition(ConditionParameter parameter, ConditionValue value);
+    }
+}
