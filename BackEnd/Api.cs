0ee8c33b0bdc406c73f23fe0f83182f1dca5655b
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-01-23 15:19:30 +0100

    Removed unused imports

--- BackEnd/Api.cs
+++ BackEnd/Api.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Collections;
-using Newtonsoft.Json;
-using System.Linq;
 using Newtonsoft.Json.Linq;
 
 namespace BackEnd
