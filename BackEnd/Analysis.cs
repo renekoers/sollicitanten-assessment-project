3377a4e74d2d60751a344d7efe32d5f9d8bd1afb
Joost Franssen <jfranssen@sogyo.nl>
2020-01-27 11:35:44 +0100

    Make basic Analysis set-up

---
+++ BackEnd/Analysis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd
+{
+    public class Analysis
+    {
+        public int LinesOfCode
+        {
+            get
+            {
+                int lines = 0;
+                foreach (Statement statement in _statements)
+                {
+                    lines += statement.GetLines();
+                }
+                return lines;
+            }
+        }
+
+        private List<Statement> _statements;
+        public Analysis(List<Statement> statements)
+        {
+            _statements = statements;
+        }
+    }
+}
