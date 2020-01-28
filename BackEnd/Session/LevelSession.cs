34382936ecd3840245511b934e47e3f1440e3a06
Joost Franssen <jfranssen@sogyo.nl>
2020-01-28 14:53:09 +0100

    Add level solution class and a database communicator (currently mock database)

--- BackEnd/Session/LevelSession.cs
+++ BackEnd/Session/LevelSession.cs
@@ -9,7 +9,21 @@ namespace BackEnd
         protected List<LevelSolution> Solutions = new List<LevelSolution>();
         public int NumberOfAttempts => Solutions.Count;
         public int LevelNumber { get; protected set; }
-        
+        public bool Solved
+        {
+            get
+            {
+                foreach (LevelSolution solution in Solutions)
+                {
+                    if (solution.Solved)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
         public LevelSession(int levelNumber) : base()
         {
             LevelNumber = levelNumber;
--- BackEnd/Session/LevelSolution.cs
+++ BackEnd/Session/LevelSolution.cs
@@ -6,15 +6,32 @@ namespace BackEnd
 {
     public class LevelSolution
     {
-        public int Number { get; private set; }
-        public Statement Code { get; private set; }
-
-        public LevelSolution(int number, Statement statement)
+        public int LevelNumber { get; private set; }
+        private readonly Statement[] Code;
+        public bool Solved { get; private set; }
+        public int Lines
         {
-            Number = number;
-            Code = statement;
+            get
+            {
+                int lines = 0;
+                foreach (Statement statement in Code)
+                {
+                    lines += statement.GetLines();
+                }
+                return lines;
+            }
+        }
+
+        public LevelSolution(int number, Statement[] statements)
+        {
+            LevelNumber = number;
+            Code = statements;
             Puzzle puzzle = new Puzzle(Level.Get(number));
-            statement.ExecuteCommand(puzzle);
+            foreach (Statement statement in Code)
+            {
+                statement.ExecuteCommand(puzzle);
+            }
+            Solved = puzzle.Finished;
         }
     }
 }
