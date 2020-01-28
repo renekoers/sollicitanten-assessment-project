88ea65bdcf1dfa34b420fd6840646b28a2d42535
Joost Franssen <jfranssen@sogyo.nl>
2020-01-28 13:16:47 +0100

    Make setup for level solution

---
+++ BackEnd/LevelSolution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd
+{
+    public class LevelSolution
+    {
+        public int Number { get; private set; }
+        public Statement Code { get; private set; }
+
+        public LevelSolution(int number, Statement statement)
+        {
+            Number = number;
+            Code = statement;
+            Puzzle puzzle = new Puzzle(Level.Get(number));
+            statement.ExecuteCommand(puzzle);
+        }
+    }
+}
