aba247174bd7758af52606e997d27b74199050a4
Joost Franssen <jfranssen@sogyo.nl>
2020-01-29 10:21:59 +0100

    Add class util for random helper functions

---
+++ BackEnd/Util.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd
+{
+    public class Util
+    {
+        private Util() { }
+
+        public static T Min<T>(List<T> list, Func<T, int> getValue) where T : class
+        {
+            int? min = null;
+            T minT = null;
+            foreach (T t in list)
+            {
+                if (min.HasValue)
+                {
+                    if (getValue(t) < min.Value)
+                    {
+                        minT = t;
+                        min = getValue(t);
+                    }
+                }
+            }
+            return minT;
+        }
+    }
+}
