62da17d44fa6ce7244ae68e90afde7d58574692b
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-01-30 11:22:32 +0100

    VSCode launch JSON
    Sorry Dennis, I need this to run the puzzle :<

--- BackEnd/States/CharacterState.cs
+++ BackEnd/States/CharacterState.cs
@@ -7,6 +7,7 @@ namespace BackEnd
     public class CharacterState
     {
         public Direction DirectionCharacter { get; }
+        public string DirectionCharacterString => DirectionCharacter.ToString();
         public TileState Tile { get; internal set; }
         public CharacterState(Direction direction)
         {
