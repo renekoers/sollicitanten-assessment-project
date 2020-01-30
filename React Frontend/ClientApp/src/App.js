1f21adf06293436a0266e4bda5122abd127ca923
Rene Koers <rkoers@sogyo.nl>
2020-01-30 11:49:02 +0100

    Feature/mainpage

--- React Frontend/ClientApp/src/App.js
+++ React Frontend/ClientApp/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { Statement } from './components/Statement';
+import { Game } from './components/Game';
 
 import './css/custom.css'
 
@@ -12,8 +12,8 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/statement' component={Statement} />
+            <Route exact path='/' component={Home} />
+            <Route path='/gamesession' component={Game} />
       </Layout>
     );
   }
---
+++ React Frontend/ClientApp/src/components/Timer.jsx
@@ -0,0 +1,54 @@
+﻿import React, { Component } from "react";
+//import '../styles/Timer.css';
+
+export class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.updateTime(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  async updateTime() {
+    let milliseconds = 0;
+
+    await fetch("api/session/remainingtime", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(localStorage.getItem("sessionID"))
+    })
+      .then(response => response.json())
+      .then(data => {
+        milliseconds = data;
+      });
+    this.setState({ time: milliseconds });
+  }
+
+  formatTime() {
+    let milliseconds = this.state.time;
+    let minutes = Math.floor(milliseconds / 60000);
+    let seconds = Math.floor(milliseconds / 1000) % 60;
+    let formattedtime = minutes + ":" + seconds;
+    return formattedtime;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="timer-container">
+          <div className="timer">{this.formatTime()}</div>
+        </div>
+      </div>
+    );
+  }
+}
