b3ecc287f598851c930a6e1ab9a3f10973835317
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-01-31 09:19:03 +0100

    JSON class with method Serialize to serialize an object

--- React Frontend/Controllers/SessionController.cs
+++ React Frontend/Controllers/SessionController.cs
@@ -38,24 +38,4 @@ namespace React_Frontend.Controllers
             return JSON.Serialize(Api.StartLevelSession(sessionID, level));
         }
     }
-
-    internal class JSON
-    {
-        private static DefaultContractResolver contractResolver = new DefaultContractResolver
-        {
-            NamingStrategy = new CamelCaseNamingStrategy
-            {
-                OverrideSpecifiedNames = false
-            }
-        };
-        internal static string Serialize(object obj)
-        {
-            return JsonConvert.SerializeObject(obj, new JsonSerializerSettings
-            {
-                ContractResolver = contractResolver,
-                Formatting = Formatting.Indented
-            });
-        }
-
-    }
 }
