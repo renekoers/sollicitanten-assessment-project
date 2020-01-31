a6d5f183769538f6d841775d1df8b6d443dfab88
Joost Franssen <jfranssen@sogyo.nl>
2020-01-31 09:53:28 +0100

    Styling for statistics

--- React Frontend/ClientApp/src/components/Statistics.js
+++ React Frontend/ClientApp/src/components/Statistics.js
@@ -2,30 +2,7 @@
 import {
     BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip,
 } from 'recharts';
-
-/*const data = [
-    {
-        name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-    },
-    {
-        name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-    },
-    {
-        name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-    },
-    {
-        name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-    },
-    {
-        name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-    },
-    {
-        name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-    },
-    {
-        name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
-];*/
+import '../css/Statistics.css';
 
 export class Statistics extends Component {
     static displayName = Statistics.name;
@@ -47,7 +24,7 @@ export class Statistics extends Component {
             for (const [levelNumber, results] of Object.entries(this.state.data)) {
                 components.push(
                     <div className="level-chart-container" key={"C" + levelNumber}>
-                        <label className="chart-level-label" key={"L" + levelNumber}>Level {levelNumber}</label>
+                        <label className="level-chart-label" key={"L" + levelNumber}>Level {levelNumber}</label>
                         <LevelBarChart classNAme="level-chart" key={"D" + levelNumber} data={results} />
                     </div>
                 );
@@ -103,7 +80,6 @@ class LevelBarChart extends Component {
                     </AxisLabel>}
                     allowDecimals={false}
                 />
-                <Tooltip />
                 <Bar dataKey="candidates" fill="#008000" />
             </BarChart>
         );
