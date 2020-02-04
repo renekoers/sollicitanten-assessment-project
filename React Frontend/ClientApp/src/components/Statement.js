3014126073757c439322dc83cec38207a83b90dc
Rene <rene.g.koers@gmail.com>
2020-02-04 13:01:36 +0100

    Added keys to children

--- React Frontend/ClientApp/src/components/Statement.js
+++ React Frontend/ClientApp/src/components/Statement.js
@@ -1,49 +1,79 @@
-import React, { Component } from 'react';
-import '../css/statement.css'
+import React, { Component } from "react";
+import "../css/statement.css";
 
 export class Statement extends Component {
-    static displayName = Statement.name;
+  static displayName = Statement.name;
 
   constructor(props) {
-      super(props);
-      this.currentButtons = [];
-      this.currentStatements = ["MoveForward", "RotateLeft", "RotateRight", "PickUp","Drop","--While--","--If--","--Else--","--End--","CheckFront","CanMoveForward","Button","Box"];
-      this.counter = 0
-      this.state = { currentButtons: this.currentButtons, counter: 0 };
-      this.showOffline = this.showOffline.bind(this);
-      this.deleteButton = this.deleteButton.bind(this);
+    super(props);
+    this.currentButtons = [];
+    this.currentStatements = [
+      "MoveForward",
+      "RotateLeft",
+      "RotateRight",
+      "PickUp",
+      "Drop",
+      "--While--",
+      "--If--",
+      "--Else--",
+      "--End--",
+      "CheckFront",
+      "CanMoveForward",
+      "Button",
+      "Box"
+    ];
+    this.counter = 0;
+    this.state = { currentButtons: this.currentButtons, counter: 0 };
+    this.showOffline = this.showOffline.bind(this);
+    this.deleteButton = this.deleteButton.bind(this);
   }
 
-    showOffline(e) {
-        this.currentButtons.push(<button id={e.target.id + this.counter} onClick={this.deleteButton}>{e.target.id}</button>);
-        this.counter = this.counter + 1
-        this.setState({
-            currentButtons: this.currentButtons,
-            counter: this.counter
-        });
-    }
+  showOffline(e) {
+    this.currentButtons.push(
+      <button
+        id={e.target.id + this.counter}
+        onClick={this.deleteButton}
+        key={e.target.id + this.counter}
+      >
+        {e.target.id}
+      </button>
+    );
+    this.counter = this.counter + 1;
+    this.setState({
+      currentButtons: this.currentButtons,
+      counter: this.counter
+    });
+  }
 
-    deleteButton(e) {
-        this.currentButtons = this.currentButtons.filter(el => el.props.id !== e.target.id);
-        this.setState({
-            currentButtons: this.currentButtons
-        })
-    }
+  deleteButton(e) {
+    this.currentButtons = this.currentButtons.filter(
+      el => el.props.id !== e.target.id
+    );
+    this.setState({
+      currentButtons: this.currentButtons
+    });
+  }
 
   render() {
     return (
-        <div>
-            <div id="wrapper">
-                <div id="input">
-                    {this.currentStatements.map((stmt) =>
-                        <button id={stmt} onClick={this.showOffline}>{stmt}</button>
-                    )} 
-                </div>
-                <div id="output">
-                    {this.currentButtons}
-                </div>
-                <button style={{ backgroundColor: 'Pink' }} onClick={this.commitStatements}> Run puzzle! </button>
-            </div>
+      <div>
+        <div id="wrapper">
+          <div id="input">
+            {this.currentStatements.map((stmt, index) => (
+              <button id={stmt} onClick={this.showOffline} key={index}>
+                {stmt}
+              </button>
+            ))}
+          </div>
+          <div id="output">{this.currentButtons}</div>
+          <button
+            style={{ backgroundColor: "Pink" }}
+            onClick={this.commitStatements}
+          >
+            {" "}
+            Run puzzle!{" "}
+          </button>
+        </div>
       </div>
     );
   }
