8d87188770e21be677d967d92d49073f80e9fa1a
Rene <rene.g.koers@gmail.com>
2020-02-04 14:17:22 +0100

    Fixed spacing

--- React Frontend/ClientApp/src/components/Statistics.js
+++ React Frontend/ClientApp/src/components/Statistics.js
@@ -1,5 +1,14 @@
-﻿import React, { Component } from "react";
-import { BarChart, Bar, XAxis, YAxis, Cell, CartesianGrid } from "recharts";
+﻿import React, {
+	Component
+} from "react";
+import {
+	BarChart,
+	Bar,
+	XAxis,
+	YAxis,
+	Cell,
+	CartesianGrid
+} from "recharts";
 import "../css/Statistics.css";
 
 export class Statistics extends Component {
@@ -33,28 +42,34 @@ export class Statistics extends Component {
 		if (this.state.dataTally) {
 			const components = [];
 			for (const [levelNumber, results] of Object.entries(
-				this.state.dataTally
-			)) {
-				components.push(
-					<div
-						className="level-chart-container"
-						key={"C" + levelNumber}
-					>
-						<label
-							className="level-chart-label"
-							key={"L" + levelNumber}
-						>
-							Level {levelNumber}
-						</label>
-						<LevelBarChart
-							className="level-chart"
-							key={"D" + levelNumber}
-							dataTally={results}
-							dataCandidate={
-								this.state.dataCurrentCandidate[levelNumber]
-							}
-						/>
-					</div>
+					this.state.dataTally
+				)) {
+				components.push( <
+					div className = "level-chart-container"
+					key = {
+						"C" + levelNumber
+					} >
+					<
+					label className = "level-chart-label"
+					key = {
+						"L" + levelNumber
+					} >
+					Level {
+						levelNumber
+					} <
+					/label> <
+					LevelBarChart className = "level-chart"
+					key = {
+						"D" + levelNumber
+					}
+					dataTally = {
+						results
+					}
+					dataCandidate = {
+						this.state.dataCurrentCandidate[levelNumber]
+					}
+					/> <
+					/div>
 				);
 			}
 			return components;
@@ -64,21 +79,37 @@ export class Statistics extends Component {
 	}
 }
 
-const AxisLabel = ({ axisType, x, y, width, height, stroke, children }) => {
+const AxisLabel = ({
+	axisType,
+	x,
+	y,
+	width,
+	height,
+	stroke,
+	children
+}) => {
 	const isVert = axisType === "yAxis";
 	const cx = isVert ? x : x + width / 2;
 	const cy = isVert ? height / 2 + y : y + height + 10;
 	const rot = isVert ? `270 ${cx} ${cy}` : 0;
-	return (
-		<text
-			x={cx}
-			y={cy}
-			transform={`rotate(${rot})`}
-			textAnchor="middle"
-			stroke={stroke}
-		>
-			{children}
-		</text>
+	return ( <
+		text x = {
+			cx
+		}
+		y = {
+			cy
+		}
+		transform = {
+			`rotate(${rot})`
+		}
+		textAnchor = "middle"
+		stroke = {
+			stroke
+		} >
+		{
+			children
+		} <
+		/text>
 	);
 };
 
@@ -93,52 +124,79 @@ class LevelBarChart extends Component {
 				candidates: Number.parseInt(_candidates)
 			});
 		}
-		this.state = { dataTally: _dataTally };
+		this.state = {
+			dataTally: _dataTally
+		};
 	}
 
 	render() {
-		return (
-			<BarChart
-				width={300}
-				height={200}
-				data={this.state.dataTally}
-				margin={50}
-			>
-				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis
-					dataKey="lines"
-					label={{ value: "Lines of Code", position: "insideBottom" }}
-					height={40}
-				/>
-				<YAxis
-					label={
-						<AxisLabel
-							axisType="yAxis"
-							x={25}
-							y={-75}
-							width={0}
-							height={300}
-						>
-							Candidates
-						</AxisLabel>
-					}
-					allowDecimals={false}
-				/>
-				<Bar dataKey="candidates">
-					{this.state.dataTally.map((entry, index) => {
-						return (
-							<Cell
-								key={`cell-${index}`}
-								fill={
-									entry.lines == this.props.dataCandidate
-										? "#00AA00"
-										: "#8CA183"
-								}
-							/>
-						);
-					})}
-				</Bar>
-			</BarChart>
+		return ( <
+			BarChart width = {
+				300
+			}
+			height = {
+				200
+			}
+			data = {
+				this.state.dataTally
+			}
+			margin = {
+				50
+			} >
+			<
+			CartesianGrid strokeDasharray = "3 3" / >
+			<
+			XAxis dataKey = "lines"
+			label = {
+				{
+					value: "Lines of Code",
+					position: "insideBottom"
+				}
+			}
+			height = {
+				40
+			}
+			/> <
+			YAxis label = {
+				<
+				AxisLabel
+				axisType = "yAxis"
+				x = {
+					25
+				}
+				y = {
+					-75
+				}
+				width = {
+					0
+				}
+				height = {
+					300
+				} >
+				Candidates <
+				/AxisLabel>
+			}
+			allowDecimals = {
+				false
+			}
+			/> <
+			Bar dataKey = "candidates" > {
+				this.state.dataTally.map((entry, index) => {
+					return ( <
+						Cell key = {
+							`cell-${index}`
+						}
+						fill = {
+							entry.lines == this.props.dataCandidate ?
+							"#00AA00" :
+								"#8CA183"
+						}
+						/>
+					);
+				})
+			} <
+			/Bar> <
+			/BarChart>
 		);
 	}
 }
