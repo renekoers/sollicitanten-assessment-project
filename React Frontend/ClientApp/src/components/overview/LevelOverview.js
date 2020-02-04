6a7836cc70bc5eafbb9ddb5b8e3df630d6377adc
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-02-04 17:04:50 +0100

    Added pictures (solved/unsolved) to overview

--- React Frontend/ClientApp/src/components/overview/LevelOverview.js
+++ React Frontend/ClientApp/src/components/overview/LevelOverview.js
@@ -1,27 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import '../../css/overview.css';
+import '../../css/Overview.css';
+import solvedIcon from "../../img/solved.png";
+import unsolvedIcon from "../../img/unsolved.png";
 
 export function LevelOverview(props) {
   const [levelNumber, setLevelNumber] = useState(0);
   const [solved, setSolved] = useState(false);
   const [numberOfLines, setNumberOfLines] = useState(0);
   const [par, setPar] = useState(0);
-  const [classNames, setClassNames] = useState("levelOverview")
   useEffect(() => {
       setLevelNumber(props.info.levelNumber);
       setSolved(props.info.solved);
       if(props.info.solved){
           setNumberOfLines(props.info.lines);
           setPar(props.info.par)
-          setClassNames(["levelOverview", "solved"].join(' '));
-      } else {
-        setClassNames(["levelOverview", "unsolved"].join(' '));
       }
   },[]);
+  let levelIcon;
+  if(solved){
+    levelIcon = solvedIcon;
+  } else {
+    levelIcon = unsolvedIcon;
+  }
   return (
-    <div className={classNames}>
-        <Link to={'/gamesession/'+levelNumber} className={classNames}> {levelNumber}: {solved ? "Solved in " + numberOfLines + " lines. Par is " + par : "Not solved"} </Link>
-    </div>
+    <article className="levelOverview">
+          <Link to={'/gamesession/'+levelNumber} className="textLevelOverview"> {levelNumber}: {solved ? "Solved in " + numberOfLines + " lines. Par is " + par  + "." : "Not solved"}
+          <img className="levelIcon" src={levelIcon} alt={solved ? "solved" : "unsolved"}/> </Link>
+    </article>
   );
 }
