e6fe70c9a2e6ee32399c40b3052b9adb41310386
Christopher <chuijting@sogyo.nl>
2020-02-06 10:50:38 +0100

    bugfix

--- React Frontend/ClientApp/src/components/overview/LevelOverview.js
+++ React Frontend/ClientApp/src/components/overview/LevelOverview.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import '../../css/Overview.css';
+import '../../css/overview.css';
 import solvedIcon from "../../img/solved.png";
 import unsolvedIcon from "../../img/unsolved.png";
 
