498b97577dc8e4042e779ae41608c5dd7d062585
Dennis Buijsman <dbuijsman@sogyo.nl>
2020-02-18 18:10:36 +0100

    Added level sessions to candidate entity

--- BackEnd/Database/CandidateEntity.cs
+++ BackEnd/Database/CandidateEntity.cs
@@ -8,14 +8,15 @@ namespace BackEnd
 	public class CandidateEntity : Entity
 	{
 		public string Name { get; protected set; }
-		public DateTime started { get; protected set; }
-		public DateTime finished { get; protected set; }
-		public GameSession GameResults { get; set; }
+		public DateTime started { get; internal set; }
+		public DateTime finished { get; internal set; }
+		public LevelSessionEntity[] GameResults { get; internal set; }
 
 		public CandidateEntity(string name)
 		{
 			this.Name = name;
-
+			GameResults = new LevelSessionEntity[Level.TotalLevels];
 		}
+		public LevelSessionEntity GetLevelSession(int levelNumber) => GameResults[levelNumber-1];
 	}
 }
