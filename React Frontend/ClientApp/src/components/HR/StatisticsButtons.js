a4af87a6b4faf81ac7824887c6ca4b4a8f997503
René Koers <rkoers@sogyo.nl>
2020-02-21 15:01:38 +0100

    Fix styles -> css

--- React Frontend/ClientApp/src/components/HR/StatisticsButtons.js
+++ React Frontend/ClientApp/src/components/HR/StatisticsButtons.js
@@ -1,35 +1,47 @@
 import React, { useState, useEffect } from "react";
-import '../../styles/SkipButton.css'
+import "../../css/SkipButton.css";
 
-export function StatisticsButtons(props){
-    const [classNamesPrevious, setClassNamesPrevious] = useState(null);
-    const [classNamesNext, setClassNamesNext] = useState(null);
-    useEffect(() => {
-        if(props.disabledPrevious){
-            setClassNamesPrevious(["skipButton", "disabled"].join(' '));
-        } else {
-            setClassNamesPrevious("skipButton");
-        }
-    },[props.disabledPrevious])
-    
-    useEffect(() => {
-        if(props.disabledNext){
-            setClassNamesNext(["skipButton", "disabled"].join(' '));
-        } else {
-            setClassNamesNext("skipButton");
-        }
-    },[props.disabledNext])
-    
-    function previousButtonClicked(){
-        props.onClickPrevious();
-    }
-    function nextButtonClicked(){
-        props.onClickNext();
-    }
-    return (
-        <div>
-            <button disabled={props.disabledPrevious} className={classNamesPrevious} onClick={ previousButtonClicked }> Vorige kandidaat</button>
-            <button disabled={props.disabledNext} className={classNamesNext} onClick={ nextButtonClicked }> Volgende kandidaat</button>
-        </div>
-    );
+export function StatisticsButtons(props) {
+	const [classNamesPrevious, setClassNamesPrevious] = useState(null);
+	const [classNamesNext, setClassNamesNext] = useState(null);
+	useEffect(() => {
+		if (props.disabledPrevious) {
+			setClassNamesPrevious(["skipButton", "disabled"].join(" "));
+		} else {
+			setClassNamesPrevious("skipButton");
+		}
+	}, [props.disabledPrevious]);
+
+	useEffect(() => {
+		if (props.disabledNext) {
+			setClassNamesNext(["skipButton", "disabled"].join(" "));
+		} else {
+			setClassNamesNext("skipButton");
+		}
+	}, [props.disabledNext]);
+
+	function previousButtonClicked() {
+		props.onClickPrevious();
+	}
+	function nextButtonClicked() {
+		props.onClickNext();
+	}
+	return (
+		<div>
+			<button
+				disabled={props.disabledPrevious}
+				className={classNamesPrevious}
+				onClick={previousButtonClicked}
+			>
+				Vorige kandidaat
+			</button>
+			<button
+				disabled={props.disabledNext}
+				className={classNamesNext}
+				onClick={nextButtonClicked}
+			>
+				Volgende kandidaat
+			</button>
+		</div>
+	);
 }
