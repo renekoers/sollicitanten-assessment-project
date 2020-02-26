bb2099facdb669a16916cf795f797f4a78605655
DeBuysman <dbuijsman@sogyo.nl>
2020-02-26 14:31:13 +0100

    WIP

---
+++ UnitTest/SelectingIDForStatisticsTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BackEnd;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class SelectingIDForStatisticsTest
+    {
+        [TestMethod]
+        public void GetOnlyNewFinishedIDsTest()
+        {
+            
+        }
+
+    }
+}
