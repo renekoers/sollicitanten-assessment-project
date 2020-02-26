f7976e9b160a15e41b04dcf01ccd22e8896f207d
DeBuysman <dbuijsman@sogyo.nl>
2020-02-26 11:17:45 +0100

    Made TestRepo complete

--- BackEnd/Database/TestDB.cs
+++ BackEnd/Database/TestDB.cs
@@ -67,62 +67,15 @@ namespace BackEnd {
                                                                     .Select(pair => pair.Value).OrderByDescending(candidate => candidate.finished).ToList();
             return finishedCandidatesBefore.Count>0 ? finishedCandidatesBefore.First() : null;
         }
-
-        async public Task<Dictionary<int,Dictionary<string, Dictionary<int, int>>>> GetStatisticsEveryone()
-        {
-            Dictionary<string,Func<LevelSession,int>> statisticsFunctions = GetStatisticsFunctions();
-            Dictionary<int,Dictionary<string, Dictionary<int, int>>> dataEveryone = new Dictionary<int,Dictionary<string, Dictionary<int, int>>>();
-            for (int levelNumber=1; levelNumber<=Level.TotalLevels;levelNumber++)
-            {
-                Dictionary<string,Dictionary<int, int>> dataSingleLevel = new Dictionary<string, Dictionary<int, int>>();
-                foreach(KeyValuePair<string,Func<LevelSession,int>> nameDataAndFunctionToIntPair in statisticsFunctions){
-                    Func<LevelSession,int> functionStatistic = nameDataAndFunctionToIntPair.Value;
-					List<CandidateEntity> listOfCandidatesWithScores = await GetCandidatesThatSolvedLevel(levelNumber);
-                    var pairingScoreAmountCandidates = listOfCandidatesWithScores.GroupBy(candidate => functionStatistic(candidate.GameResults[levelNumber-1]))
-						.Select(result => new { Score = result.Key, AmountCandidates = result.Count() })
-                        .OrderBy(result => result.Score);
-					Dictionary<int,int> levelStatistic = pairingScoreAmountCandidates.ToDictionary(result => result.Score, result => result.AmountCandidates);
-                    dataSingleLevel.Add(nameDataAndFunctionToIntPair.Key, levelStatistic);
-                }
-                dataEveryone.Add(levelNumber, dataSingleLevel);
-            }
-            return dataEveryone;
-        }
-        async public Task<List<CandidateEntity>> GetCandidatesThatSolvedLevel(int levelNumber)
+        async public Task<List<CandidateEntity>> GetListOfCandidatesWithScores(int levelNumber)
         {
             await new Task(() => {});
             return TestRepo.Where(pair => pair.Value.finished > new DateTime() && pair.Value.GameResults[levelNumber-1].Solved).Select(pair => pair.Value).ToList();
         }
-
-        async public Task<Dictionary<int,int>> GetStatisticsUnsolved()
-        {
-            Dictionary<int,int> amountUnsolved = new Dictionary<int, int>();
-            for(int levelNumber=1; levelNumber<=Level.TotalLevels;levelNumber++)
-            {
-				amountUnsolved[levelNumber] = await GetAmountCandidatesThatNotSolvedLevel(levelNumber);
-            }
-            return amountUnsolved;
-        }
-        async public Task<int> GetAmountCandidatesThatNotSolvedLevel(int levelNumber)
+        async public Task<int> GetAmountUnsolved(int levelNumber)
         {
             await new Task(() => {});
             return TestRepo.Where(pair => pair.Value.finished > new DateTime() && !pair.Value.GameResults[levelNumber-1].Solved).Count();
         }
-
-
-        /// <summary>
-        /// Creates a list of functions that maps a levelSession to an int. This function are used in constructing dictionaries that represents the statistics.
-        /// Add here extra functions in order to add extra statistics.
-        /// </summary>
-        /// <returns>Dictionary with for name of the statistic creates data that represents the statistic of the given level.</returns>
-        private static Dictionary<string,Func<LevelSession,int>> GetStatisticsFunctions()
-        {
-            Dictionary<string,Func<LevelSession,int>> statisticsFunctions = new Dictionary<string,Func<LevelSession, int>>();
-            statisticsFunctions.Add("Regels code kortste oplossing", LevelSession.GetLines);
-            statisticsFunctions.Add("Tijd tot korste oplossing", LevelSession.GetDuration);
-            statisticsFunctions.Add("Pogingen tot korste oplossing", session => session.NumberOfAttemptsForFirstSolved);
-            return statisticsFunctions;
-        }
-
     }
 }
