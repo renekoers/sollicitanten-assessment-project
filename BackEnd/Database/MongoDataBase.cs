2332f726267741fa91105b3fd6698365529e96b6
Aleksandar <asimatovic@sogyo.nl>
2020-02-26 10:27:18 +0100

    Remove logic from mongodatabase classes about statistics

--- BackEnd/Database/MongoDataBase.cs
+++ BackEnd/Database/MongoDataBase.cs
@@ -79,40 +79,19 @@ namespace BackEnd {
         /// Creates a dictionary consisting of all statistics of all candidates.
         /// </summary>
         /// <returns>Dictionary with for every level has a dictionary of name of the statistic and the combination of data and number of candidates.</returns>
-        async public Task<Dictionary<int,Dictionary<string, Dictionary<int, int>>>> GetStatisticsEveryone(){
-            Dictionary<string,Func<LevelSession,int>> statisticsFunctions = GetStatisticsFunctions();
-            Dictionary<int,Dictionary<string, Dictionary<int, int>>> dataEveryone = new Dictionary<int,Dictionary<string, Dictionary<int, int>>>();
-            for (int levelNumber=1; levelNumber<=Level.TotalLevels;levelNumber++)
-            {
-                Dictionary<string,Dictionary<int, int>> dataSingleLevel = new Dictionary<string, Dictionary<int, int>>();
-                foreach(KeyValuePair<string,Func<LevelSession,int>> nameDataAndFunctionToIntPair in statisticsFunctions){
-                    Func<LevelSession,int> functionStatistic = nameDataAndFunctionToIntPair.Value;
-					List<CandidateEntity> listOfCandidatesWithScores = await MongoDB.Collection<CandidateEntity>().AsQueryable()
-						.Where(candidate => candidate.finished > new DateTime() && candidate.GameResults[levelNumber-1].Solved)
-						.ToListAsync();
-                    var pairingScoreAmountCandidates = listOfCandidatesWithScores.GroupBy(candidate => functionStatistic(candidate.GameResults[levelNumber-1]))
-						.Select(result => new { Score = result.Key, AmountCandidates = result.Count() })
-                        .OrderBy(result => result.Score);
-					Dictionary<int,int> levelStatistic = pairingScoreAmountCandidates.ToDictionary(result => result.Score, result => result.AmountCandidates);
-                    dataSingleLevel.Add(nameDataAndFunctionToIntPair.Key, levelStatistic);
-                }
-                dataEveryone.Add(levelNumber, dataSingleLevel);
-            }
-            return dataEveryone;
+        async public Task<List<CandidateEntity>> GetListOfCandidatesWithScores(int levelNumber){
+            return await MongoDB.Collection<CandidateEntity>().AsQueryable()
+                .Where(candidate => candidate.finished > new DateTime() && candidate.GameResults[levelNumber-1].Solved)
+                .ToListAsync();
         }
 
         /// <summary>
         /// This method creates a dictionary consisting of the number of candidates that did not solve a certain level.
         /// </summary>
         /// <returns></returns>
-        async public Task<Dictionary<int,int>> GetStatisticsUnsolved(){
-            Dictionary<int,int> amountUnsolved = new Dictionary<int, int>();
-            for(int levelNumber=1; levelNumber<=Level.TotalLevels;levelNumber++)
-            {
-				amountUnsolved[levelNumber] = await MongoDB.Collection<CandidateEntity>().AsQueryable()
+        async public Task<int> GetAmountUnsolved(int levelNumber){
+            return await MongoDB.Collection<CandidateEntity>().AsQueryable()
 					.CountAsync(candidate => candidate.finished > new DateTime() && !candidate.GameResults[levelNumber-1].Solved);
-            }
-            return amountUnsolved;
         }
 
         /// <summary>
