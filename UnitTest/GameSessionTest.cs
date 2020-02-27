c4b96e53af6a6601559eb1c943144e8dabf1e955
DeBuysman <dbuijsman@sogyo.nl>
2020-02-27 11:03:50 +0100

    Fixed UnitTests
    Rebuild to whole UnitTest folder..............

---
+++ UnitTest/GameSessionTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BackEnd;
+using React_Frontend.Controllers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class GameSessionTest
+    {
+        [TestMethod]
+        public async Task ControllerAcceptsValidIDTest()
+        {
+            IRepository repo = new TestDB();
+            GameSessionController controller = new GameSessionController(repo);
+            string id = await repo.AddCandidate("Test");
+            ActionResult result = await controller.StartSession(id);
+            Assert.IsTrue(result is OkResult);
+        }
+    }
+}
--- UnitTest/StateTest.cs
+++ UnitTest/StateTest.cs
@@ -1,4 +1,4 @@
-﻿// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
 // using BackEnd;
 // using System.Collections.Generic;
 
