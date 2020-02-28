2ade539b80fc880bc59d875a177974b7201347d1
Aleksandar <asimatovic@sogyo.nl>
2020-02-28 14:14:09 +0100

    WIP

--- UnitTest/HRController.cs
+++ UnitTest/HRController.cs
@@ -17,12 +17,33 @@ namespace UnitTest {
         private HRController hrController = new HRController(new TestDB());
         
         [TestMethod]
-        public void LoginTest(){
-            // JsonElement credentials = (JsonElement) JSON.Deserialize("\"username\":\"testUsername\",\"password\":\"testPassword\"");
-            // ActionResult<string> response = hrController.Login(credentials);
+        public async Task LoginTest(){
+            JsonElement credentials = await (JsonElement) JSON.Serialize("\"username\":\"testUsername\",\"password\":\"testPassword\"");
+            ActionResult<string> response = hrController.Login(credentials);
             // string token = JWT.CreateToken("testUsername", "testPassword");
+            // ActionResult<string> msg = JSON.Serialize("Bearer " + token);
 
-            Assert.AreEqual("response","response");
+            Assert.AreEqual(response, response);
         }
+
+        [TestMethod]
+        public async Task AddCandidateSuccess() {
+            var response = await hrController.AddCandidate("testadd");
+            Assert.IsInstanceOfType(response, typeof(OkResult));
+        }
+
+         [TestMethod]
+        public async Task AddCandidateEmptyStringAsName() {
+            var response = await hrController.AddCandidate("");
+            Assert.IsInstanceOfType(response, typeof(UnprocessableEntityResult));
+        }
+
+         [TestMethod]
+        public async Task AddCandidateNameIsNull() {
+            var response = await hrController.AddCandidate(null);
+            Assert.IsInstanceOfType(response, typeof(UnprocessableEntityResult));
+        }
+
+        
     }
 }
