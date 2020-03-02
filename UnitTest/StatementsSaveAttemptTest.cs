f48a492e7a6ec4ab6c893bd128e19a21923a4724
DeBuysman <dbuijsman@sogyo.nl>
2020-03-02 13:20:19 +0100

    Made tests for the statementscontroller

--- UnitTest/StatementsSaveAttemptTest.cs
+++ UnitTest/StatementsSaveAttemptTest.cs
@@ -84,5 +84,55 @@ namespace UnitTest
             List<LevelSolution> solutions = candidate.GetLevelSession(levelNumber).Solutions;
             Assert.AreEqual(0, solutions.Count);
         }
+        
+        [TestMethod]
+        public async Task SaveStatementsAcceptsStatementsTest()
+        {
+            int levelNumber = 1;
+            IRepository repo = new TestDB();
+            StatementController controller = new StatementController(repo);
+            GameSessionController gameController = new GameSessionController(repo);
+            LevelSessionController levelController = new LevelSessionController(repo);
+            string id = await repo.AddCandidate("Test");
+            await gameController.StartSession(id);
+            await levelController.StartLevel(id, levelNumber.ToString());
+            ActionResult<string> result = await controller.SaveAttempt(id, levelNumber, new Statement[]{new SingleCommand(Command.RotateLeft)});
+            Assert.IsTrue(result.Result == null || ((StatusCodeResult) result.Result).StatusCode == 200);
+        }
+        
+        [TestMethod]
+        public async Task SaveStatementsSavesAttemptTest()
+        {
+            int levelNumber = 1;
+            IRepository repo = new TestDB();
+            StatementController controller = new StatementController(repo);
+            GameSessionController gameController = new GameSessionController(repo);
+            LevelSessionController levelController = new LevelSessionController(repo);
+            string id = await repo.AddCandidate("Test");
+            await gameController.StartSession(id);
+            await levelController.StartLevel(id, levelNumber.ToString());
+            await controller.SaveAttempt(id, levelNumber, new Statement[]{new SingleCommand(Command.RotateLeft)});
+            CandidateEntity candidate = await repo.GetCandidate(id);
+            List<LevelSolution> solutions = candidate.GetLevelSession(levelNumber).Solutions;
+            Assert.AreEqual(1, solutions.Count);
+        }
+        
+        [TestMethod]
+        public async Task SaveStatementsSavesDuplicateAttemptTest()
+        {
+            int levelNumber = 1;
+            IRepository repo = new TestDB();
+            StatementController controller = new StatementController(repo);
+            GameSessionController gameController = new GameSessionController(repo);
+            LevelSessionController levelController = new LevelSessionController(repo);
+            string id = await repo.AddCandidate("Test");
+            await gameController.StartSession(id);
+            await levelController.StartLevel(id, levelNumber.ToString());
+            await controller.SaveAttempt(id, levelNumber, new Statement[]{new SingleCommand(Command.RotateLeft)});
+            await controller.SaveAttempt(id, levelNumber, new Statement[]{new SingleCommand(Command.RotateLeft)});
+            CandidateEntity candidate = await repo.GetCandidate(id);
+            List<LevelSolution> solutions = candidate.GetLevelSession(levelNumber).Solutions;
+            Assert.AreEqual(2, solutions.Count);
+        }
     }
 }
