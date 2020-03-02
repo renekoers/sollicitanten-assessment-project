c238d9108074d8d4519427cd23a1099a554541f5
DeBuysman <dbuijsman@sogyo.nl>
2020-03-02 11:13:56 +0100

    WIP - making tests for creating statistics

--- UnitTest/StatisticsTest.cs
+++ UnitTest/StatisticsTest.cs
@@ -1,13 +1,59 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using Microsoft.AspNetCore.Mvc;
-// using BackEnd;
-// using React_Frontend.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using BackEnd;
+using React_Frontend.Controllers;
 
-// namespace UnitTest
-// {
-//     [TestClass]
-//     public class StatisticsTest
-//     {
+namespace UnitTest
+{
+    [TestClass]
+    public class StatisticsTest
+    {
+        [TestMethod]
+        public async Task MakeStatisticsOfCandidateRejectsIfIdIsIncorrect()
+        {
+            IRepository repo = new TestDB();
+            StatisticsController controller = new StatisticsController(repo);
+            string id = await repo.AddCandidate("Test");
+            ActionResult<string> result = await controller.MakeStatisticsCandidate(id + "NOT");
+            Assert.AreNotEqual(((ObjectResult) result.Result).StatusCode, 200);
+        }
         
-//     }
-// }
+        [TestMethod]
+        public async Task MakeStatisticsOfCandidateRejectsIfCandidateIsNotFinished()
+        {
+            IRepository repo = new TestDB();
+            StatisticsController controller = new StatisticsController(repo);
+            string id = await repo.AddCandidate("Test");
+            await (new GameSessionController(repo)).StartSession(id);
+            ActionResult<string> result = await controller.MakeStatisticsCandidate(id);
+            Assert.AreNotEqual(((ObjectResult) result.Result).StatusCode, 200);
+        }
+        
+        [TestMethod]
+        public async Task MakeStatisticsOfCandidateAcceptIfCandidateIsFinished()
+        {
+            IRepository repo = new TestDB();
+            StatisticsController controller = new StatisticsController(repo);
+            GameSessionController gameSessionController = new GameSessionController(repo);
+            string id = await repo.AddCandidate("Test");
+            await gameSessionController.StartSession(id);
+            await gameSessionController.EndSession(id);
+            ActionResult<string> result = await controller.MakeStatisticsCandidate(id);
+            Assert.AreEqual(((ObjectResult) result.Result).StatusCode, 200);
+        }
+
+        [TestMethod]
+        public async Task MakeStatisticsCandidateGivesOnlyStatisticsOfSolvedLevelsTest()
+        {
+            IRepository repo = new TestDB();
+            StatisticsController controller = new StatisticsController(repo);
+            GameSessionController gameSessionController = new GameSessionController(repo);
+            LevelSessionController levelSessionController = new LevelSessionController(repo);
+            string id = await repo.AddCandidate("Test");
+            await gameSessionController.StartSession(id);
+            // WIP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            await gameSessionController.EndSession(id);
+        }
+    }
+}
