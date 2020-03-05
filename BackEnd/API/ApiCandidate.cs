e5ee8b119b79c7173b15e2bb9bf6f7f3750bbbf2
DeBuysman <dbuijsman@sogyo.nl>
2020-03-05 14:10:43 +0100

    Removed unused code

--- BackEnd/API/ApiCandidate.cs
+++ BackEnd/API/ApiCandidate.cs
@@ -6,46 +6,9 @@ namespace BackEnd
 {
 	public partial class Api
 	{
-		async public static Task<CandidateEntity> GetCandidate()
-		{
-			return await Database.GetCandidate();
-		}
-		async public static Task<CandidateEntity> GetCandidate(string ID)
-		{
-			return await Database.GetCandidate(ID);
-		}
-		async public static Task<bool> IsUnstarted(string ID)
-		{
-			return await Database.IsUnstarted(ID);
-		}
 		async public static Task<IEnumerable<CandidateEntity>> GetAllUnstartedCandidate()
 		{
 			return await Database.GetAllUnstartedCandidate();
-		}
-		/// Checks property of candidate
-		
-		///<summary>
-		/// Returns true if candidate has yet to start a session.
-		///</summary>
-		///<param name="ID"></param>
-		///<returns>Bool</returns>
-		async public static Task<bool> HasCandidateNotYetStarted(string ID)
-		{
-			return await Database.HasCandidateNotYetStarted(ID);
-		}
-		async public static Task<bool> IsStarted(string ID)
-		{
-			CandidateEntity candidate = await Database.GetCandidate(ID);
-			return candidate != null && candidate.IsStarted();
-		}
-		///<summary>
-		/// Returns true if candidate still has an active session available.
-		///</summary>
-		///<param name="ID"></param>
-		///<returns>Bool</returns>
-		async public static Task<bool> IsCandidateStillActive(string ID)
-		{
-			return await Database.IsCandidateStillActive(ID);
 		}
     }
 }
--- BackEnd/Database/Database.cs
+++ BackEnd/Database/Database.cs
@@ -21,53 +21,10 @@ namespace BackEnd
 			await MongoDB.SaveAsync(candidate);
 			return candidate.ID;
 		}
-		async internal static Task<CandidateEntity> GetCandidate()
-		{
-			List<CandidateEntity> unstartedCandidates = await MongoDB.Find<CandidateEntity>()
-				.Match(candidate => candidate.started == new DateTime())
-				.Limit(1)
-				.ExecuteAsync();
-			return unstartedCandidates.Count != 0 ? unstartedCandidates.First() : null;
-		}
-
-		async internal static Task<CandidateEntity> GetCandidate(string ID)
-		{
-			try
-			{
-				return await MongoDB.Find<CandidateEntity>().OneAsync(ID);
-			} catch(Exception error)
-			{
-				Console.WriteLine("Can't find any user with ID: " + ID);
-				Console.WriteLine(error.StackTrace);
-				return null;
-			}
-		}
 		async internal static Task<IEnumerable<CandidateEntity>> GetAllUnstartedCandidate()
 		{
 			return (await MongoDB.Find<CandidateEntity>()
 			.ManyAsync(a => a.started == new DateTime())).ToList();
-		}
-
-		async internal static Task<bool> HasCandidateNotYetStarted(string ID)
-		{
-			CandidateEntity candidate = await GetCandidate(ID);
-			// Checks if candidate exists && candidate is not started && candidate is not finished.
-			return candidate != null && !candidate.IsStarted() && !candidate.IsFinished();
-		}
-
-		async internal static Task<bool> IsCandidateStillActive(string ID)
-		{
-			CandidateEntity candidate = await GetCandidate(ID);
-			// Checks if candidate exists && candidate is started && candidate is not finished.
-			return candidate != null && candidate.IsStarted() && !candidate.IsFinished();
-		}
-		[Obsolete]
-		/// This function checks if a candidate is started instead of unstarted. This method is the same as HasCandidateNotYetStarted
-		async internal static Task<bool> IsUnstarted(string ID)
-		{
-			CandidateEntity candidate = await GetCandidate(ID);
-			// Checks if candidate exists && candidate is started
-			return candidate != null && !candidate.IsStarted();
 		}
 	}
 }
