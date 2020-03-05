988a2c81c0c33ba2e61a7d162cdb70a29b34bba8
DeBuysman <dbuijsman@sogyo.nl>
2020-03-05 15:07:53 +0100

    Finished tests candidate controller

--- UnitTest/CandidateTest.cs
+++ UnitTest/CandidateTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using BackEnd;
 using React_Frontend.Controllers;
 
@@ -12,9 +13,9 @@ namespace UnitTest
         [TestMethod]
         async public Task GetCandidateRejectsIfIdIsNotGivenTest()
         {
-            IRepository _repo = new TestDB();
-            CandidateController controller = new CandidateController(_repo);
-            await _repo.AddCandidate("Test");
+            IRepository repo = new TestDB();
+            CandidateController controller = new CandidateController(repo);
+            await repo.AddCandidate("Test");
             ActionResult<string> result = await controller.getCandidateName(null);
             Assert.AreNotEqual(200, ((StatusCodeResult) result.Result).StatusCode);
         }
@@ -22,9 +23,9 @@ namespace UnitTest
         [TestMethod]
         async public Task GetCandidateRejectsIfIdIsInvalidTest()
         {
-            IRepository _repo = new TestDB();
-            CandidateController controller = new CandidateController(_repo);
-            string id = await _repo.AddCandidate("Test");
+            IRepository repo = new TestDB();
+            CandidateController controller = new CandidateController(repo);
+            string id = await repo.AddCandidate("Test");
             ActionResult<string> result = await controller.getCandidateName(id + "NOT");
             Assert.AreNotEqual(200, ((StatusCodeResult) result.Result).StatusCode);
         }
@@ -32,9 +33,9 @@ namespace UnitTest
         [TestMethod]
         async public Task GetCandidateAcceptsIfIdIsValidTest()
         {
-            IRepository _repo = new TestDB();
-            CandidateController controller = new CandidateController(_repo);
-            string id = await _repo.AddCandidate("Test");
+            IRepository repo = new TestDB();
+            CandidateController controller = new CandidateController(repo);
+            string id = await repo.AddCandidate("Test");
             ActionResult<string> result = await controller.getCandidateName(id);
             Assert.IsTrue(result.Result == null || ((StatusCodeResult) result.Result).StatusCode == 200);
         }
@@ -42,9 +43,9 @@ namespace UnitTest
         [TestMethod]
         async public Task GetCandidateReturnsTheRightCandidateTest()
         {
-            IRepository _repo = new TestDB();
-            CandidateController controller = new CandidateController(_repo);
-            string id = await _repo.AddCandidate("Test");
+            IRepository repo = new TestDB();
+            CandidateController controller = new CandidateController(repo);
+            string id = await repo.AddCandidate("Test");
             ActionResult<string> result = await controller.getCandidateName(id);
             CandidateEntity candidate = JSON.Deserialize<CandidateEntity>(result.Value);
             Assert.AreEqual(id, candidate.ID);
@@ -53,9 +54,9 @@ namespace UnitTest
         [TestMethod]
         async public Task CannotGetStatusOfAnNonExistingCandidateTest()
         {
-            IRepository _repo = new TestDB();
-            CandidateController controller = new CandidateController(_repo);
-            string id = await _repo.AddCandidate("Test");
+            IRepository repo = new TestDB();
+            CandidateController controller = new CandidateController(repo);
+            string id = await repo.AddCandidate("Test");
             ActionResult<string> result = await controller.getStatus(id + "NOT");
             Assert.AreNotEqual(200, ((StatusCodeResult) result.Result).StatusCode);
         }
@@ -63,11 +64,45 @@ namespace UnitTest
         [TestMethod]
         async public Task StatusNotStartedCandidateTest()
         {
-            IRepository _repo = new TestDB();
-            CandidateController controller = new CandidateController(_repo);
-            string id = await _repo.AddCandidate("Test");
+            IRepository repo = new TestDB();
+            CandidateController controller = new CandidateController(repo);
+            string id = await repo.AddCandidate("Test");
             ActionResult<string> result = await controller.getStatus(id);
-            
+            var definition = new {started = false, finished = false};
+            var status = JsonConvert.DeserializeAnonymousType(result.Value, definition);
+            Assert.IsFalse(status.started);
+            Assert.IsFalse(status.finished);
+        }
+
+        [TestMethod]
+        async public Task StatusCandidateWithActiveSessionTest()
+        {
+            IRepository repo = new TestDB();
+            CandidateController controller = new CandidateController(repo);
+            GameSessionController gameSessionController = new GameSessionController(repo);
+            string id = await repo.AddCandidate("Test");
+            await gameSessionController.StartSession(id);
+            ActionResult<string> result = await controller.getStatus(id);
+            var definition = new {started = false, finished = false};
+            var status = JsonConvert.DeserializeAnonymousType(result.Value, definition);
+            Assert.IsTrue(status.started);
+            Assert.IsFalse(status.finished);
+        }
+
+        [TestMethod]
+        async public Task StatusFinishedCandidateTest()
+        {
+            IRepository repo = new TestDB();
+            CandidateController controller = new CandidateController(repo);
+            GameSessionController gameSessionController = new GameSessionController(repo);
+            string id = await repo.AddCandidate("Test");
+            await gameSessionController.StartSession(id);
+            await gameSessionController.EndSession(id);
+            ActionResult<string> result = await controller.getStatus(id);
+            var definition = new {started = false, finished = false};
+            var status = JsonConvert.DeserializeAnonymousType(result.Value, definition);
+            Assert.IsTrue(status.started);
+            Assert.IsTrue(status.finished);
         }
     }
 }
